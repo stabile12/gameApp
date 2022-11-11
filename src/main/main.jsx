02a837b99cd3ea5fd5fdf840143565bd54e2d427
stabile12 <jestabilerocha@gmail.com>
2022-11-11 02:52:10 -0300

    main button update

--- src/main/main.jsx
+++ src/main/main.jsx
@@ -6,12 +6,9 @@ export const Main = () => {
   const [nome, setNome] = useState('')
   return (
     <div className='container-main'>
-      <p className='title'>Olá! Diga seu nome e escolha um jogo para começar!</p>
-      <input type="text" className="name-input" onChange={(e) => setNome(e.target.value)} />
+      <p className='title'>Olá! Escolha um jogo para começar!</p>
       <Link className='main-button' to={'/velha'}><button className='main-button' >Jogo da velha</button></Link>
       <Link className='main-button' to={'/forca'}><button className='main-button'>Jogo da Forca</button></Link>
-      
-
     </div>
 
 
